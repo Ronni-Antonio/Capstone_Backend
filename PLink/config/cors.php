f3597375fcdb773cb3954b9d718a82de4cfeddd8
Rhoanne Nicole Antonio <antonio_rhoannenicole@plpasig.edu.ph>
2026-09-01 23:16:45 +0800

    PDF download fix

--- PLink/config/cors.php
+++ PLink/config/cors.php
@@ -2,12 +2,12 @@
 
 return [
 
-'paths' => ['api/*', 'sanctum/csrf-cookie'],
-'allowed_methods' => ['*'],
-'allowed_origins' => ['http://localhost:5173'],
-'allowed_origins_patterns' => [],
-'allowed_headers' => ['*'],
-'exposed_headers' => [],
-'max_age' => 0,
-'supports_credentials' => false,
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['https://d1v2rn4nvcvtip.cloudfront.net/'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
 ];
